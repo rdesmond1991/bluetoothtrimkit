d966e2a0f03446f0a4c00540b9deb26642d74bf5
Ross Desmond <rdesmond91@gmail.com>
2023-12-30 12:20:10 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Paragliding Trim Kit`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
